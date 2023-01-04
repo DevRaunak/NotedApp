e8dc763f890b277032250f5cb62d4746615bdf9f
raunakpurohit <raunak.wscubetech@gmail.com>
2023-01-04 17:51:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/prodroid/notedapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.prodroid.notedapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
